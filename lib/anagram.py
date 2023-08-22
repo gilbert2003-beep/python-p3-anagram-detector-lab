e23e4fa70c84e3fc0abec82c3bf686330e8abe2b
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-22 12:33:01 +0300

    passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        result = []
+        for candidate in word_list:
+            if self.is_anagram(candidate):
+                result.append(candidate)
+        return result
+
+    def is_anagram(self, candidate):
+        return sorted(candidate.lower()) == sorted(self.word.lower()) and candidate.lower() != self.word.lower()
